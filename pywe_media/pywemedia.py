92942872774c2005cad69e2d073881738aed3ac9
Brightcells <brightcells@gmail.com>
2018-07-26 17:04:57 +0800

    :art: Add media_file_path for media_upload

--- pywe_media/pywemedia.py
+++ pywe_media/pywemedia.py
@@ -13,7 +13,7 @@ class Media(BaseToken):
         # 高清语音素材获取接口, Refer: https://mp.weixin.qq.com/wiki?t=resource/res_main&id=mp1444738727
         self.WECHAT_MEDIA_GET_JSSDK = self.API_DOMAIN + '/cgi-bin/media/get/jssdk?access_token={access_token}&media_id={media_id}'
 
-    def upload(self, media_type='image', media_file=None, appid=None, secret=None, token=None, storage=None):
+    def upload(self, media_type='image', media_file=None, media_file_path=None, appid=None, secret=None, token=None, storage=None):
         """
         :param media_type: 媒体文件类型，分别有图片（image）、语音（voice）、视频（video）和缩略图（thumb）
         :param media_file: 要上传的文件，一个 File-object
@@ -25,7 +25,7 @@ class Media(BaseToken):
                 'type': media_type,
             },
             files={
-                'media': media_file,
+                'media': media_file or open(media_file_path, 'rb'),
             },
         )
 
--- tests/test_media.py
+++ tests/test_media.py
@@ -26,8 +26,8 @@ class TestMediaCommands(object):
     def test_media_upload_download(self):
         appid = WECHAT.get('JSAPI', {}).get('appID')
         appsecret = WECHAT.get('JSAPI', {}).get('appsecret')
-        media_file = open(IMAGE_PATH, 'rb')
-        data = media_upload(media_file=media_file, appid=appid, secret=appsecret)
+        # media_file = open(IMAGE_PATH, 'rb')
+        data = media_upload(media_file=None, media_file_path=IMAGE_PATH, appid=appid, secret=appsecret)
         # {u'media_id': u'KptzZFv35-iSWE6cgmsQpaeqw986IjcFlqumgm_IEkP3-8-wyMDJNw12H4b4RPVP', u'created_at': 1503983273, u'type': u'image'}
         assert isinstance(data, dict)
         assert data.get('media_id', '')
